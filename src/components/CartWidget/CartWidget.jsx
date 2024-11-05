35c0872d176c057fd2a062d4ca93419bfdd9cb39
wifrons <wifron@gmail.com>
2024-11-05 15:45:59 -0300

    ajuste en ruta de imagenes

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -2,7 +2,7 @@
 export default function CartWidget({ amount }) {
     return (
         <a className="nav-link">
-            <img style={{ width: "32px" }} src="/public/icon/sales_cart.png" className="logoCart" alt="logoCart"/>
+            <img style={{ width: "32px" }} src="icon/sales_cart.png" className="logoCart" alt="logoCart"/>
             <span className="badge text-bg-secondary">{amount}</span>
         </a>
     )
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,7 +5,7 @@ export default function NavBar() {
         <nav className="navbar navbar-expand-lg bg-body-tertiary" >
             <div className="container-fluid" style={{ backgroundColor: "#d3d5d7", padding:"10px" }}>
                 <a className="nav-link" style={{ marginRight: '200px' }}>
-                    <img style={{ width: '96px' }} src="/public/icon/icoremar_logo.png" className="logo icoremar" alt="logo" />
+                    <img style={{ width: '96px' }} src="icon/icoremar_logo.png" className="logo icoremar" alt="logo" />
                     <span style={{ fontSize: '32px' }}>iCoremar</span>
                 </a>
                 <div className="collapse navbar-collapse" id="navbarNav" style={{ alignItems: "center", justifyContent: "flex-end" }}>
